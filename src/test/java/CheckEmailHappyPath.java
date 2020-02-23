7ff74b530f0c93296e1ce7d467e2fd52164d75ae
VHubernatorov <59209364+VHubernatorov@users.noreply.github.com>
2020-02-23 21:12:38 +0200

    Update CheckEmailHappyPath.java

--- src/test/java/CheckEmailHappyPath.java
+++ src/test/java/CheckEmailHappyPath.java
@@ -1,29 +1,37 @@
+package com.lipsum;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CheckEmailHappyPath {
+    Email email = new Email();
+    boolean flag = email.isEmailCorrect("poor@gmail.com");
+    boolean flag1 = email.isEmailCorrect("po1234or@gm1al.com");
+    boolean flag3 = email.isEmailCorrect("Po12or3@Mail2.com");
 
-    @DataProvider (name = "Email")
-    public static Object[][] dataMetod() {
-        return new Object[][]{
-                {"Ad1@gmail.com"},
-                {"weq51@gmail.com"},
-                {"fluffy_2003@gmail.com"},
-                {"behappy!@gmail.com"},
-                {"behappy!@12l.com"}
-        };
+
+    @DataProvider(name = "Authentication")
+    public static Object[] credentials() {
+        return new Object[] { "poor@gmail.com", "po1234or@gm1al.com", "Po12or3@Mail2.com"};
+    }
+    @Test
+    public void checkEmail(){
+        Assert.assertEquals(true, flag);
+    }
+    @Test
+    public void checkEmail2(){
+        Assert.assertEquals(true, flag1);
+    }
+    @Test
+    public void checkEmail3(){
+        Assert.assertEquals(true, flag3);
     }
 
-    @Test(dataProvider = "Email")
-    public static void checkemailunhappypath(String str) {
-        Email e = new Email();
-        boolean resul = e.isEmailCorrect(str);
-        Assert.assertTrue (resul);
-
+    @Test(dataProvider = "Authentication")
+    public void checkEmailArray(String str){
+        boolean flag4 = email.isEmailCorrect(str);
     }
+
+
 }
-
-
-
